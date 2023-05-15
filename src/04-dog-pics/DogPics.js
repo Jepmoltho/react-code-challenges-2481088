eb7f66979f849449a75d5fd54e86456d79cc7dd3
Jepmoltho <jept@itu.dk>
2023-05-15 19:15:01 +0200

    Exe 01_04 complete

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,23 @@
-export default function DogPics () {
+import { useState } from "react";
+
+export default function DogPics() {
+  const [dawg, setDawg] = useState(
+    "https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg"
+  );
   // API: https://dog.ceo/dog-api/
+  async function getRandomDawg() {
+    //const APIbaseUrl = "https://dog.ceo/api/breeds/image/random";
+    const response = await fetch(
+      "https://dog.ceo/api/breeds/image/random"
+    ).then((humlebuks) => humlebuks.json());
+
+    setDawg(response.message);
+  }
+
   return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+    <div className="dog-pics">
+      <img src={dawg} />
+      <button onClick={() => getRandomDawg()}>🐶</button>
     </div>
-  )
+  );
 }
