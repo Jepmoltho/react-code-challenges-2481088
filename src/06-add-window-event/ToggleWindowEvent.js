1c81603bac73562d90ec963cf71c42c0bd8522bf
Jepmoltho <jept@itu.dk>
2023-05-19 15:15:23 +0200

    Correct way of hiding/displaying components

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import WindowEvent from './WindowEvent'
+import { useState } from "react";
+import WindowEvent from "./WindowEvent";
 
-export default function ToggleWindowEvent () {
-  const [windowEvent, setWindowEvent] = useState(false)
+export default function ToggleWindowEvent() {
+  const [windowEvent, setWindowEvent] = useState(false);
+
+  function returnWindowEvent() {
+    if (windowEvent) {
+      return <WindowEvent />;
+    }
+  }
+
   return (
     <div>
-      <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
-      {windowEvent && <WindowEvent />}
+      <button onClick={() => setWindowEvent((prevState) => !prevState)}>
+        Toggle Window Event
+      </button>
+      {returnWindowEvent()}
     </div>
-  )
+  );
 }
