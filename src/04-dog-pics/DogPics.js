9425df4fc2ce4fa6e3a43115cae138738dc28011
Jepmoltho <jept@itu.dk>
2023-06-07 20:41:37 +0200

    Final branch into dev

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import { useEffect, useState } from 'react'
-
-const getDogPic = async () => {
-  const response = await fetch('https://dog.ceo/api/breeds/image/random')
-  const dog = await response.json()
-  return dog.message
-}
-
-export default function DogPics () {
-  const [dogPic, setDogPic] = useState('')
-
-  useEffect(() => {
-    getDogPic().then(dogPic => setDogPic(dogPic))
-  }, [])
-
-  return (
-    <div className='dog-pics'>
-      <img src={dogPic} />
-      <button onClick={async e => setDogPic(await getDogPic())}>🐶</button>
-=======
 import { useState, useEffect } from "react";
 
 export default function DogPics() {
@@ -41,7 +20,6 @@ export default function DogPics() {
     <div className="dog-pics">
       <img src={dawg} />
       <button onClick={() => getRandomDawg()}>🐶</button>
->>>>>>> 01_04b
     </div>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,70 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import ShoppingCart from "./11-shopping-cart/ShoppingCart";
 import ShoppingCartExpected from "./11-shopping-cart/ShoppingCartExpectedSolution";
-=======
-import "./App.css";
-//import ToggleWindowEvent from "./06-add-window-event/ToggleWindowEvent";
-import ToggleWindowEventFreestyle from "./06-add-window-event/ToggleWindowEvent";
->>>>>>> 01_06b
-=======
-import "./App.css";
-import PixelArt from "./08-pixel-art/PixelArt";
-import PixelArtContext from "./08-pixel-art/PixelArtContext";
-import PixelArtFreeStyle from "./08-pixel-art/PixelArtFreestyle";
->>>>>>> 01_08b
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
       <ShoppingCartExpected />
-=======
-      <ToggleWindowEventFreestyle />
->>>>>>> 01_06b
-=======
-      <PixelArtFreeStyle />
-      <PixelArt />
-      <PixelArtContext />
->>>>>>> 01_08b
     </div>
   );
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import "./App.css";
-import DarkMode from "./02-dark-mode/DarkMode";
-
-function switchToDarkMode() {
-  document.getElementsByClassName("App")[0].classList.add("dark-mode");
-  document.getElementsByClassName("App")[0].classList.remove("light-mode");
-  console.log("Dark fire");
-}
-
-function switchToLightMode() {
-  document.getElementsByClassName("App")[0].classList.add("light-mode");
-  document.getElementsByClassName("App")[0].classList.remove("dark-mode");
-  console.log("Light fire");
-}
-
-function App() {
-  return (
-    <div className="App">
-      <DarkMode
-        switchToDarkMode={() => switchToDarkMode()}
-        switchToLightMode={() => switchToLightMode()}
-      />
-    </div>
-  );
-}
-
->>>>>>> 01_02b
-=======
->>>>>>> 01_06b
-=======
->>>>>>> 01_08b
 export default App;
